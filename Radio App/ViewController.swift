ad1f091436be7a4c550db3a1e9d419ea58357257
Md Abdul Awal <masyam.cse@gmail.com>
2020-10-14 02:11:04 +0200

    Update ViewController.swift

--- Radio App/ViewController.swift
+++ Radio App/ViewController.swift
@@ -13,22 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var volumeSlider: UISlider!
     @IBOutlet weak var gifImage: UIImageView!
     @IBOutlet weak var playButton: UIButton!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
-    
-    //let urltest = URL(string: "http:www.kpftx.org:8000/wuvi_32.m3u")
-  
-    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView! 
     
     var avPlayer:AVPlayer?
     var avPlayerItem:AVPlayerItem?
+    var observer:Any!
     
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        print("Device-Name: "+UIDevice.current.modelName);
-        
+        super.viewDidLoad()                
         UIApplication.shared.isIdleTimerDisabled = true
         
         let defaults = UserDefaults.standard
@@ -37,58 +30,32 @@ class ViewController: UIViewController {
         }
         
         //set uibutton scale aspect
-        playButton.imageView?.contentMode = .scaleAspectFill
-        
-        //activityIndicator.startAnimating();
+        playButton.imageView?.contentMode = .scaleAspectFill        
         self.activityIndicator.hidesWhenStopped = true;
         
-        //Example URL
-        //let urlstring = "http://www.noiseaddicts.com/samples_1w72b820/2514.mp3"
-        let urlstring = "" //Here goes your audio streaming url
+        //Example URL "http://www.noiseaddicts.com/samples_1w72b820/2514.mp3"
+        let urlstring = ""
         let url = NSURL(string: urlstring)
-        print("playing \(String(describing: url))")
         
         avPlayerItem = AVPlayerItem.init(url: url! as URL)
         avPlayer = AVPlayer.init(playerItem: avPlayerItem)
         avPlayer?.volume = UserDefaults.standard.float(forKey: "volumeValue")
-        volumeSlider.value = UserDefaults.standard.float(forKey: "volumeValue")
-        //avPlayer?.play()
-        
-        //playButton.addTarget(self, action: #selector(ViewController.playButtonTapped), for: .touchUpInside)
-        
-        
+        volumeSlider.value = UserDefaults.standard.float(forKey: "volumeValue")                
         
         self.observer = avPlayer?.addPeriodicTimeObserver(forInterval: CMTimeMake(1, 600), queue: DispatchQueue.main) {
-            [weak self] time in
-            
-            if self?.avPlayer?.currentItem?.status == AVPlayerItemStatus.readyToPlay {
-                
-               if(self?.activityIndicator.isAnimating)!
-               {
+            [weak self] time in           
+            if self?.avPlayer?.currentItem?.status == AVPlayerItemStatus.readyToPlay {                
+               if(self?.activityIndicator.isAnimating)! {
                  self?.activityIndicator.stopAnimating();
                  self?.gifImage.isHidden = false;
                  self?.gifImage.loadGif(name: "live-logo")
                }
             }
-        }
-        
-        
-    
+        }       
     }
     
-    
-    
-    
-   // @objc func playButtonTapped(sender: AnyObject) {
-    //}
-    
-    
-    var observer:Any!
-    
-    
-    
-    @IBAction func VolumeChanged(_ sender: UISlider) {
-        
+    //Button Actions
+    @IBAction func VolumeChanged(_ sender: UISlider) {        
         let currentValue = Float(sender.value)
         avPlayer?.volume = Float(currentValue)
         UserDefaults.standard.set(currentValue, forKey: "volumeValue")
@@ -97,28 +64,22 @@ class ViewController: UIViewController {
    
     
     
-    @IBAction func PlayButtonTapped(_ sender: UIButton) {
-        
-        if avPlayer?.rate == 0
-        {
+    @IBAction func PlayButtonTapped(_ sender: UIButton) {    
+        if avPlayer?.rate == 0{
             self.activityIndicator.startAnimating()
             avPlayer!.play()
-            playButton.setImage(UIImage(named: "pause.png"), for: [])
-            
-        } else {
-            
+            playButton.setImage(UIImage(named: "pause.png"), for: [])         
+        } else {           
             self.activityIndicator.stopAnimating();
             gifImage.isHidden = true;
             avPlayer!.pause()
             playButton.setImage(UIImage(named: "play.png"), for: [])
-        }
-        
+        }       
     }
     
     
     override var shouldAutorotate: Bool {
-        return false
-        
+        return false        
     }
     
     
@@ -130,7 +91,5 @@ class ViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }  
-
-
 }
 
